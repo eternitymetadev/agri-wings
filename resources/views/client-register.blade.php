b40bbbecd6a7ef82659950c4f7fa4c82f0d25af3
eternitywebdev <dev.eternitysolutions@gmail.com>
2023-04-08 10:42:55 +0530

    adsf

--- resources/views/client-register.blade.php
+++ resources/views/client-register.blade.php
@@ -255,7 +255,7 @@
                     </div> -->
 
                     <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">Laravel Google Recaptcha</label>
+                            <label class="col-md-4 col-form-label text-md-right"></label>
                             <div class="col-md-6"> {!! htmlFormSnippet() !!} </div>
                         </div>
                     
